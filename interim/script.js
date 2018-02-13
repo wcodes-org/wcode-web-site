d9f303d4d678fbbf68e3ed1000bceb4d50036e78
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-02-14 04:21:59 +0530

    Publish

--- interim/script.js
+++ interim/script.js
@@ -1,40 +1,23 @@
-function nowLoading() {
-	var e = document.getElementById("loading");
-	if(nowLoading.count === undefined) {
-		nowLoading.count = 0;
-		resetLoadingText(e);
-		e.setAttribute("style", "visibility: visible");
-	}
-	else {
-		if(nowLoading.count == 3)
-			resetLoadingText(e);
-		else {
-			e.innerText += ".";
-			nowLoading.count++;
-		}
-	}
-}
-
 function beginLoading() {
-	clearTimeout(intrvl); // ensure single timer
-	intrvl = setInterval(nowLoading, 1000);
-}
-
-function resetLoadingText(e) {
-	e.innerText = "Loading";
-}
-
-function errorLoading() {
-	var e = document.getElementById("loading");
-	e.setAttribute("style", "visibility: visible");
-	e.innerText = "Error!";
+	document.getElementById("wait_loader").classList.remove('hide');
 }
 
 function endLoading() {
-	var e = document.getElementById("loading");
-	nowLoading.count = undefined;
-	e.setAttribute("style", "visibility: hidden");
-	clearTimeout(intrvl);
+	document.getElementById("wait_loader").classList.add('hide');
+}
+
+function fadeLoading() {
+	setTimeout(function() {
+		endLoading();
+	}, 3000);
+}
+
+function errorLoading() {
+	document.getElementById("notification").classList.remove('hide');
+	clearTimeout(intrvl); // ensure single timer
+	intrvl = setInterval(function() {
+		document.getElementById("notification").classList.add('hide');
+	},3000);
 }
 
 function fbReload() {
@@ -644,6 +627,7 @@ function about_me() {
 }
 function presentation() {
 	beginLoading();
+	fadeLoading();
 }
 var wordListArray = [];
 
