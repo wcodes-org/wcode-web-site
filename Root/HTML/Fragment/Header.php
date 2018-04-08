2424e897daba75da82eaec2057b1db4f2c2ea12a
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-08 17:14:12 +0530

    Slogan and title text
    Made it less technical and more human

--- Root/HTML/Fragment/Header.php
+++ Root/HTML/Fragment/Header.php
@@ -2,7 +2,7 @@
 	<div id='header-title'>
 	<div class='XURL' data-target='root' id='header-slogan'>
 		<a id='header-slogan-href' href='/'>
-			<span id='header-slogan-text'>enCode with Words</span>
+			<span id='header-slogan-text'>convey with words</span>
 		</a>
 	</div>
 	<div class='XURL' data-target='root' id='header-logo'>
