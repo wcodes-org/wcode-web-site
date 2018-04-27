030e320d0005f8141d255507ea9ee03d00fae509
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-28 04:23:17 +0530

    div in p

--- Root/HTML/Component/In_pictures.php
+++ Root/HTML/Component/In_pictures.php
@@ -1,6 +1,6 @@
-	<p>
-		<div class="image-desc">A mobile no. 9801234567 encoded into WCode - \ Cat Apple Mango Tomato /</div>
 <div id='message'>
+	<div>
+		<p class="image-desc">A mobile no. 9801234567 encoded into WCode - \ Cat Apple Mango Tomato /</p>
 		<div class="center">
 		<div>
 			<img src="/resource/comics_1.png" alt="WCode number example - comics" />
@@ -11,12 +11,12 @@
 			<img src="/resource/comics_4.png" alt="WCode number example - comics" />
 		</div>
 		</div>
-	</p>
-	<p>
-		<div class="image-desc">Typically to be used when a human being is in between - two disconnected computer systems</div>
+	</div>
+	<div>
+		<p class="image-desc">Typically to be used when a human being is in between - two disconnected computer systems</p>
 		<div class="center">
 			<img src="/resource/general-use_case.png" alt="WCode general use-case" />
 		</div>
-	</p>
+	</div>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
