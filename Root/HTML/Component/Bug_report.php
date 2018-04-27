d917832c845e202fdf941e9902a51551ba09706e
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-28 04:31:13 +0530

    Formatting

--- Root/HTML/Component/Bug_report.php
+++ Root/HTML/Component/Bug_report.php
@@ -2,12 +2,12 @@
 	<h1><strong>Known Bugs &amp; Resolution history:</strong></h1>
 	<p>
 		v1.0
-		<ol class='indent-40'>
-			<li>
-				Mode: Raw-Decimal<br>
-				A number larger than 64bits fails.
-			</li>
-		</ol>
 	</p>
+	<ol class='indent-40'>
+		<li>
+			Mode: Raw-Decimal<br>
+			A number larger than 64bits fails.
+		</li>
+	</ol>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
