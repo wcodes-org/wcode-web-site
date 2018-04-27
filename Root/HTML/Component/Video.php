4f22d853d2a1d4d5c2e12ca77812aa11d90b3691
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-28 04:17:46 +0530

    Formatting

--- Root/HTML/Component/Video.php
+++ Root/HTML/Component/Video.php
@@ -1,6 +1,6 @@
-	<div class="indent-16">
-	<p>WCode Introduction:</p>
 	<iframe class="video" width="640" height="392" src="https://www.youtube.com/embed/LfGeIWCeeKU?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>	</div>
 <div id='message'>
+	<div class='indent-16'>
+		<h3>WCode introduction</h3>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
