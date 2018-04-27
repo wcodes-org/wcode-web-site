03487ebb4dbfeecfbfc83fb97d9c6972937ef72d
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-28 04:18:41 +0530

    video : embed code improved and fluid size

--- Root/HTML/Component/Video.php
+++ Root/HTML/Component/Video.php
@@ -1,6 +1,9 @@
-	<iframe class="video" width="640" height="392" src="https://www.youtube.com/embed/LfGeIWCeeKU?feature=player_detailpage" frameborder="0" allowfullscreen></iframe>	</div>
 <div id='message'>
 	<div class='indent-16'>
 		<h3>WCode introduction</h3>
+		<div class='video-wrapper'>
+			<iframe class='center' src='https://www.youtube.com/embed/LfGeIWCeeKU' allow="autoplay; encrypted-media" allowfullscreen></iframe>
+		</div>
+	</div>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
