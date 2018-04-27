f1a61c0ad7d3c0cc63ffd9453173221d47d3b715
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-28 04:40:38 +0530

    Added alt for img tag and formatting

--- Root/HTML/Component/Logo.php
+++ Root/HTML/Component/Logo.php
@@ -1,60 +1,61 @@
-<table id="logo-table">
-	<tbody>
-		<tr>
-			<td><img src="/logo_small.png" /></td>
-			<td>
-				Logo<br>
-				<br>
-				<a class="content-link indent-10" href="/logo_small.svg">
-					SVG
-				</a>
-			</td>
-		</tr>
-		<tr>
-			<td><img src="/logo_full.png" /></td>
-			<td>
-				Logo-Full<br>
-				<br>
-				<a class="content-link indent-10" href="/logo_full.svg">
-					SVG
-				</a>
-			</td>
-		</tr>
-	</tbody>
-</table>
 <div id='message'>
+	<table id='logo-table'>
+		<tbody>
+			<tr>
+				<td><img src='/logo_small.png' alt="WCode small logo"></td>
+				<td>
+					Logo<br>
+					<br>
+					<a class="content-link indent-10" href="/logo_small.svg">
+						SVG
+					</a>
+				</td>
+			</tr>
+			<tr>
+				<td><img src='/logo_full.png' alt="WCode full logo"></td>
+				<td>
+					Logo-Full<br>
+					<br>
+					<a class="content-link indent-10" href='/logo_full.svg'>
+						SVG
+					</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 
-<table id="logo-part-table">
-	<tbody>
-		<tr>
-			<td><img src="/resource/logo_w.png" /></td>
-			<td>
-				'W'<br>
-				&ndash;for <strong>Words</strong>
-			</td>
-		</tr>
-		<tr>
-			<td><img src="/resource/logo_n.png" /></td>
-			<td>
-				'N'<br>
-				&ndash;for <strong>Nouns</strong><br>
-				A break after the left arm - before the rising arm to the middle of the 'W'
-			</td>
-		</tr>
-		<tr>
-			<td><img src="/resource/logo_backslash.png" /></td>
-			<td>
-				' \ '<br>
-				&ndash;for WCodes begin with a <strong>backslash</strong>
-			</td>
-		</tr>
-		<tr>
-			<td><img src="/resource/logo_fwdslash.png" /></td>
-			<td>
-				' / '<br>
-				&ndash;as WCodes end with a <strong>forwardslash</strong>
-			</td>
-		</tr>
-	</tbody>
-</table>
+	<table id='logo-part-table'>
+		<tbody>
+			<tr>
+				<td><img src='/resource/logo_w.png' alt="WCode logo - isolated W part"></td>
+				<td>
+					W<br>
+					&ndash;for <strong>Words</strong>
+				</td>
+			</tr>
+			<tr>
+				<td><img src='/resource/logo_n.png' alt="WCode logo - isolated N part"></td>
+				<td>
+					N<br>
+					&ndash;for <strong>Nouns</strong><br>
+					A break after the left arm - before the rising arm to the middle of the 'W'
+				</td>
+			</tr>
+			<tr>
+				<td><img src='/resource/logo_backslash.png' alt="WCode logo - isolated backslash"></td>
+				<td>
+					\<br>
+					&ndash;for WCodes begin with a <strong>backslash</strong>
+				</td>
+			</tr>
+			<tr>
+				<td><img src='/resource/logo_fwdslash.png' alt="WCode logo - isolated forwardslash"></td>
+				<td>
+					/<br>
+					&ndash;as WCodes end with a <strong>forwardslash</strong>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 <?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
