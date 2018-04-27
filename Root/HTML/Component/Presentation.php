51b5001d4da6ce1a25a746c434580d2f085e2cf6
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-28 04:22:12 +0530

    Additional id removed and clean html

--- Root/HTML/Component/Presentation.php
+++ Root/HTML/Component/Presentation.php
@@ -1,4 +1,4 @@
-	<iframe id="presentation-frame" src="https://docs.google.com/presentation/embed?id=1sYiXCWIYAWpKAahrGFZ2p5zJX8uMxPccu-oaGOajrGA&start=false&loop=false&delayms=3000" frameborder="0" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true" id="g-presentation"></iframe>
 <div id='message'>
+	<iframe id="g-presentation" src="https://docs.google.com/presentation/embed?id=1sYiXCWIYAWpKAahrGFZ2p5zJX8uMxPccu-oaGOajrGA&start=false&loop=false&delayms=3000" allowfullscreen></iframe>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
