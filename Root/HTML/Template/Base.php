42e90960920e00c8cd0ed58a32cb3f32ddabcfc6
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-29 03:27:45 +0530

    Formatting

--- Root/HTML/Template/Base.php
+++ Root/HTML/Template/Base.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" itemscope="" itemtype="http://schema.org/WebPage">
+<html xmlns='http://www.w3.org/1999/xhtml' lang='en' itemscope='' itemtype='http://schema.org/WebPage'>
 <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" >
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
-	<meta name="title" content="<?php echo $config['project_title'] ?>" >
-	<meta name="author" content="<?php echo $config['author'] ?>" >
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+	<meta http-equiv='X-UA-Compatible' content='IE=edge' >
+	<meta http-equiv='Content-Type' content="text/html; charset=UTF-8" >
+	<meta name='title' content="<?php echo $config['project_title'] ?>" >
+	<meta name='author' content="<?php echo $config['author'] ?>" >
+	<meta name='viewport' content="width=device-width, initial-scale=1.0" >
 	<?php require '../HTML/Fragment/Google_Plus_Meta.php' ?>
 	<?php require '../HTML/Fragment/OG_Meta.php' ?>
 	<?php require '../HTML/Fragment/FB_Meta.php' ?>
 	<?php require '../HTML/Fragment/Twitter_Meta.php' ?>
-	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" >
-	<link rel="apple-touch-icon" type="image/png" href="/apple-touch-icon.png" >
+	<link rel="shortcut icon" type='image/x-icon' href='/favicon.ico' >
+	<link rel='apple-touch-icon' type='image/png' href='/apple-touch-icon.png' >
 	<link rel='manifest' href='/manifest.json' >
 	<link href="<?php echo $config['base_url']; if($id != 'root') echo '/'.$id ?>" rel='canonical' >
 	<title>
@@ -21,7 +21,7 @@
 	</title>
 <?php
 	if($bPublish) {
-		require "../JS/Fragment/GA_HeadScript.php";
+		require '../JS/Fragment/GA_HeadScript.php';
 	}
 	require '../JS/Fragment/GTranslate.php';
 	require '../JS/Fragment/GCSE.php';
@@ -35,20 +35,20 @@
 		require '../JS/Fragment/GA_track.js';
 		require '../JS/Fragment/BodyBegin_FB.php';
 ?>
-		<script async src="//apis.google.com/js/platform.js" defer></script>
-		<script async src="//platform.twitter.com/widgets.js"></script>
+		<script async src='//apis.google.com/js/platform.js' defer></script>
+		<script async src='//platform.twitter.com/widgets.js'></script>
 <?php
 	}
 ?>
 	<div id='wait_loader' class='hide'></div>
 	<div id='main-wrapper' class="<?php echo $menu_active_class; echo ($id == 'root'? ' '.'hide_path_title_updated' : ' ') ?>">
-		<div id="content-wrapper">
+		<div id='content-wrapper'>
 		<?php require '../../HTML/Fragment/Header.php'; ?>
-		<div id="wrapper">
-			<div id="main">
-				<div class="container">
-					<div id="content-wrapper-inside">
-						<div class="shadow-scroll-top"></div>
+		<div id='wrapper'>
+			<div id='main'>
+				<div class='container'>
+					<div id='content-wrapper-inside'>
+						<div class='shadow-scroll-top'></div>
 						<div id='google_translate_element'></div>
 						<?php require '../HTML/Fragment/GCSE.php' ?>
 						<div id='canvas-wrapper'>
