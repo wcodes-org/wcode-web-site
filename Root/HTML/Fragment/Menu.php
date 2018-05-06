300648432dd83c9778da0f9d4b73f63581ee176b
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-05-07 05:08:07 +0530

    Added component: patent

--- Root/HTML/Fragment/Menu.php
+++ Root/HTML/Fragment/Menu.php
@@ -7,7 +7,7 @@
 		<div class='sidebar-nav-li sidebar-sub'>
 <?php
 			$MENU_MAX_ITEM_COUNT = -5;
-			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['root', ''], ['video', 'Video'], ['downloads', 'Downloads'], ['faq', 'FAQ'], ['in_pictures', 'In pictures'], ['pitch', 'Pitch'], ['use_cases', 'Use cases'], ['applications', 'Applications'], ['about', 'About']);
+			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['root', ''], ['video', 'Video'], ['downloads', 'Downloads'], ['faq', 'FAQ'], ['in_pictures', 'In pictures'], ['pitch', 'Pitch'], ['use_cases', 'Use cases'], ['applications', 'Applications'], ['about', 'About'], ['patent', 'Patent']);
 			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['adoption', 'Adoption'], ['bug_report', 'Bug report'], ['changelog', 'Changelog'], ['comparision', 'Comparision'], ['data_set', 'Data set'], ['features', 'Features'], ['feedback', 'Feedback'], ['guidelines', 'Guidelines'], ['implementations', 'Implementations'], ['license', 'License'], ['logo', 'Logo'], ['presentation', 'Presentation'], ['roadmap', 'Roadmap'], ['source_code', 'Source code'], ['technology', 'Technology'], ['timeline', 'Timeline'], ['user_guide', 'User guide'], ['wordlist', 'WordList']);
 			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['about_project', 'About project'], ['about_site', 'About site'], ['about_me', 'About me'], ['clientele', 'Clientele'], ['media', 'Media']);
 ?>
