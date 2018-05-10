abd396b74bc17c2bfe7d9489565a97ac52e6646f
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-05-10 14:32:41 +0530

    Added location

---
+++ Root/HTML/Component/Location/Index.php
@@ -0,0 +1,63 @@
+<div id='message'>
+	<div class='alt_div_shade'>
+		<div class='top-bottom-gap'>
+			<?php includeSVG($id, 'Map'); ?>
+		</div>
+		<div>
+			<h2><span class="bullet">•</span>Problem</h2>
+			<div class="indent-60">
+				<p>
+					Even in the GPS age, why do addresses have to be so complicated ?
+				</p>
+				<p>
+					They are generally too non-standardized, lengthy and ambiguous.<br>
+					Also, often very imprecise.
+				</p>
+				<p>
+					And even the latitude and longitude coordinates are too complex numbers to be used by the general public.<br>
+				</p>
+			</div>
+		</div>
+		<div>
+			<h2><span class="bullet">•</span>Solution</h2>
+			<div class="indent-60">
+				<p>
+					Not any more! Now addresses can be super-short and simple.
+				</p>
+				<p>
+					The WCode location app can take you to any point in a city using just three very simple words.
+				</p>
+				<p>
+					Basically,<br>
+					It’s a PIN code to your door, but of words.
+				</p>
+			</div>
+		</div>
+		<div>
+			<h2><span class="bullet"></span>Comparision</h2>
+			<div class="indent-60">
+				<p>
+					Other competing systems use alphabets and numbers.<br>
+					For end users they don’t help much
+				</p>
+				<p>
+					One system uses words but their dictionary comprises of thousands of words and thus includes difficult pronunciation and spellings - defeating the very purpose
+				</p>
+				<p>
+					<strong>WCode location uses only 1024 basic words</strong>
+				</p>
+			</div>
+		</div>
+		<div>
+			<h2><span class="bullet"></span>App</h2>
+			<div class="indent-60">
+				<p>
+					Click on the link below to go to the webapp and watch it simplify your address:
+				</p>
+			</div>
+		</div>
+	</div>
+	<?php group_image("page-list center", 1, ['app/location', 'WCode location', 'http://wcodes.org']) ?>
+	<?php require('..\HTML\Fragment\SubList.php') ?>
+</div>
+<?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
