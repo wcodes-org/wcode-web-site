85883f16c62ec1ed218362353d799d05ed29ef99
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-05-04 05:27:51 +0530

    Changed video url to that from dedicated channel

--- Root/HTML/Component/Video.php
+++ Root/HTML/Component/Video.php
@@ -2,7 +2,7 @@
 	<div class='indent-16'>
 		<h3>WCode introduction</h3>
 		<div class='video-wrapper'>
-			<iframe class='center' src='https://www.youtube.com/embed/LfGeIWCeeKU' allow="autoplay; encrypted-media" allowfullscreen></iframe>
+			<iframe class='center' src='https://www.youtube.com/embed/yJFI-bg11M8' allow="autoplay; encrypted-media" allowfullscreen></iframe>
 		</div>
 	</div>
 </div>
