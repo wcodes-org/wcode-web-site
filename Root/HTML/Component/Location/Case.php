38a33d6bacb3878c0ba7bc7d5c25733d1388f6fc
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-08-31 23:20:49 +0530

    Location page segregated further into 'presentation' and 'case'

---
+++ Root/HTML/Component/Location/Case.php
@@ -0,0 +1,10 @@
+<div id='message'>
+	<div>
+		<?php includeSVG($id, 'Case_1'); ?>
+		<?php includeSVG($id, 'Case_2'); ?>
+		<?php includeSVG($id, 'Case_3'); ?>
+		<?php includeSVG($id, 'Case_4'); ?>
+		<?php includeSVG($id, 'Case_5'); ?>
+	</div>
+</div>
+<?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
---
+++ Root/HTML/Component/Location/Competition.php
@@ -0,0 +1,6 @@
+<div id='message'>
+	<div>
+		<?php includeSVG($id, 'Competition_1'); ?>
+	</div>
+</div>
+<?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
