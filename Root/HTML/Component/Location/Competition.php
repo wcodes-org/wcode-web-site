425e1b1f94aca587a6a9e7cb5dc7f0858406d9a1
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-09-13 01:38:11 +0530

    Corrected 'competition' image name

--- Root/HTML/Component/Location/Competition.php
+++ Root/HTML/Component/Location/Competition.php
@@ -1,6 +1,6 @@
 <div id='message'>
 	<div>
-		<?php includeSVG($id, 'Competition_1'); ?>
+		<?php includeSVG($id, 'comparision'); ?>
 	</div>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
