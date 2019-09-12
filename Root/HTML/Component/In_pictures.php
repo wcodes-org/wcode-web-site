77607446ab750469ec3dd800e4a5b6ab6356e1f5
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-09-13 03:12:42 +0530

    Formatting

--- Root/HTML/Component/In_pictures.php
+++ Root/HTML/Component/In_pictures.php
@@ -1,7 +1,7 @@
 <div id='message'>
 	<div>
-		<p class="image-desc">A mobile no. <span class="blue">9801234567</span> encoded into WCode - <span class="blue">\ Cat Apple Mango Tomato /</span></p>
-		<div class="center">
+		<p class='image-desc'>A mobile no. <span class='blue'>9801234567</span> encoded into WCode - <span class='blue'>\ Cat Apple Mango Tomato /</span></p>
+		<div class='center'>
 		<div>
 			<img src="/resource/comics_1.png" alt="WCode number example - comics" />
 			<img src="/resource/comics_2.png" alt="WCode number example - comics" />
@@ -13,8 +13,8 @@
 		</div>
 	</div>
 	<div>
-		<p class="image-desc">Typically to be used when a human being is in between - two disconnected computer systems</p>
-		<div class="center">
+		<p class='image-desc'>Typically to be used when a human being is in between - two disconnected computer systems</p>
+		<div class='center'>
 			<img src="/resource/general-use_case.png" alt="WCode general use-case" />
 		</div>
 	</div>
