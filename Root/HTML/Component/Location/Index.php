427440f5be128d797bcc4a34e4555075956baf4b
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-10-30 11:19:51 +0530

    Disabled video autoplay

--- Root/HTML/Component/Location/Index.php
+++ Root/HTML/Component/Location/Index.php
@@ -15,7 +15,7 @@
 		
 		<div class='video-container' id='location_video'>
 			<div class='video-wrapper'>
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/-9Ws_0ibvPA?autoplay=1&rel=0&modestbranding=1&enablejsapi=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/-9Ws_0ibvPA?autoplay=0&rel=0&modestbranding=1&enablejsapi=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 			</div>
 		</div>
 
