f100af8e38321208f7dfeb3d1bd9431fcb084323
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-11-26 00:02:54 +0530

    Moved 'gray' and 'additional' list to the top

--- Root/File/Render-lists.ps1
+++ Root/File/Render-lists.ps1
@@ -1,6 +1,6 @@
-Get-Content 'Wordlist - Graylist.tsv' | Select-Object -Skip 1 > Wordlist-gray.tsv
-cat 'Wordlist - Whitelist.tsv', Wordlist-gray.tsv > Wordlist.tsv
-rm Wordlist-gray.tsv
+Get-Content 'Wordlist - Whitelist.tsv' | Select-Object -Skip 1 > Wordlist-white.tsv
+cat 'Wordlist - Graylist.tsv', Wordlist-white.tsv > Wordlist.tsv
+rm Wordlist-white.tsv
 (Get-Content Wordlist.tsv -Raw) -replace "(?m)^\s*`r`n",'' | Set-Content Wordlist.tsv
 (Get-Content 'Wordlist - Blacklist.tsv' -Raw) -replace "(?m)^\s*`r`n",'' | Set-Content Wordlist-black.tsv
 rm 'Wordlist - Graylist.tsv'
