6cefa8a051154fe29c843cea38af188e46b6106e
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-02-26 03:57:41 +0530

    Updating script reference change in all html files

--- post-publish.ps1
+++ post-publish.ps1
@@ -2,6 +2,10 @@
 
 $crc = get-crc32 ./public/script.js
 mv ./public/script.js ./public/script-$crc.js
-$file = ".\public\index.html"
-$text = (Get-Content -Path $file -ReadCount 0) -join "`n"
-$text -replace '<script src="/script', ('<script src="/script-'+$crc) | Set-Content -Path $file
+
+Get-ChildItem ".\public\" -R -Filter *.html | 
+Foreach-Object {
+	$file = $_.fullName
+	$text = (Get-Content -Path $file -ReadCount 0)
+	$text -replace "src='/script.js", ("<script src='/script-"+$crc+".js") | Set-Content -NoNewline -Path $file
+}
