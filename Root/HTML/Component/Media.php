c4b680966fe0950c0e53af5b22a6a20fb4ab0b33
Ujjwal Singh <ujjwalsingh@outlook.com>
2021-05-05 05:04:18 +0530

    Added Media:NYK Daily - Wolo Codes launch link

--- Root/HTML/Component/Media.php
+++ Root/HTML/Component/Media.php
@@ -1,5 +1,5 @@
 <div id='message'>
 	<p>WCode in Media</p>
-	<span class="gray content-low indent-a">&ndash;empty&ndash;</span>
+	<span class="content-low indent-a"><a class='content-link' href='https://nykdaily.com/2021/03/wolo-codes-app-is-now-taking-gps-technology-to-new-levels'>NYK Daily: Wolo Codes app is now taking GPS technology to new levels</a></span>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom_nav.php') ?>
